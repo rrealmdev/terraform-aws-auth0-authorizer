7dd199d2b4bda938a4c39d1390c1688cf0ddfa5c
Jonny Pillar <info@jonnypillar.co.uk>
2020-11-21 18:49:50 +0000

    Remove String Interpolation From Outputs

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,24 @@
 output lambda_arn {
   description = "API Lambda ARN."
-  value       = "${aws_lambda_function.lambda.arn}"
+  value       = aws_lambda_function.lambda.arn
 }
 
 output lambda_name {
   description = "API Lambda name."
-  value       = "${aws_lambda_function.lambda.function_name}"
+  value       = aws_lambda_function.lambda.function_name
 }
 
 output lambda_role_arn {
   description = "Lambda role ARN."
-  value       = "${aws_iam_role.role.arn}"
+  value       = aws_iam_role.role.arn
 }
 
 output lambda_role_name {
   description = "Lambda role name."
-  value       = "${aws_iam_role.role.name}"
+  value       = aws_iam_role.role.name
 }
 
 output lambda_role_policy_arns {
   description = "Lambda role policy ARNs."
-  value       = ["${aws_iam_role_policy_attachment.lambda_policies.*.policy_arn}"]
+  value       = [aws_iam_role_policy_attachment.lambda_policies.*.policy_arn]
 }
